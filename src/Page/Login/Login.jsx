b3a8f3a41f7969f9437683ce36fb949e94e588ce
Ashfakul Islam <r4pido3@gmail.com>
2023-12-21 15:15:13 +0600

    log page working

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,8 +1,42 @@
 import Lottie from "lottie-react";
 import loginAni from "../../assets/loginAni.json";
 import SocialLogin from "../../Components/SocialLogin/SocialLogin";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import useAuth from "../../Components/hooks/useAuth/useAuth";
+import Swal from "sweetalert2";
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const { signInUser } = useAuth();
+
+    const navigate = useNavigate()
+
+  const onSubmit = (data) => {
+    const email = data.email;
+    const password = data.password;
+
+    signInUser(email, password)
+      .then(() => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Logged in successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate("/");
+      })
+      .catch((err) => {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: `${err.message}`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      });
+  };
   return (
     <div className="min-h-[calc(100vh-155px)]">
       <h1 className="text-5xl font-bold text-center">Log In</h1>
@@ -11,11 +45,12 @@ const Login = () => {
           <Lottie animationData={loginAni} loop={true} />
         </div>
         <div className="w-full space-y-2">
-          <form className="space-y-5">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
             <div className="space-y-2">
               <h3 className="font-semibold text-lg">Email</h3>
               <input
                 type="email"
+                {...register("email")}
                 placeholder="Type here email here"
                 className="input input-bordered w-full"
               />
@@ -24,6 +59,7 @@ const Login = () => {
               <h3 className="font-semibold text-lg">Password</h3>
               <input
                 type="password"
+                {...register("password")}
                 placeholder="Type here password here"
                 className="input input-bordered w-full"
               />
@@ -37,7 +73,12 @@ const Login = () => {
             <h1 className="text-center text-2xl font-bold">Login With</h1>
             <SocialLogin />
           </div>
-          <p className="mt-2 text-center">Do not have an account? <Link to={'/register'}><span className="font-bold text-blue-500">Register</span></Link></p>
+          <p className="mt-2 text-center">
+            Do not have an account?{" "}
+            <Link to={"/register"}>
+              <span className="font-bold text-blue-500">Register</span>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
